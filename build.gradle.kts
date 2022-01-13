d49efb57ccaad8bd3e7504c442a751207f859654
Mati Del Bel <matiasdelbel@gmail.com>
2022-01-13 19:52:10 +0100

    Create project structure

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+
+    repositories {
+        google()
+        mavenCentral()
+
+        maven("https://plugins.gradle.org/m2/") /* Needed for DetectKt */
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
+        classpath("com.android.tools.build:gradle:7.0.4")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+
+        maven("https://plugins.gradle.org/m2/") /* Needed for DetectKt */
+
+        maven { setUrl("https://jitpack.io") }
+    }
+}
---
+++ plugin/build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    `java-gradle-plugin`
+    `kotlin-dsl`
+    `maven-publish`
+}
+
+// TODO -> move plugin publishing to another file.
+group = "com.delbel.lego"
+version = "1.0.0"
+
+publishing {
+    publications {
+
+        repositories {
+            mavenLocal()
+        }
+
+        create<MavenPublication>("maven") {
+            artifactId = "plugin"
+
+            from(components["kotlin"])
+
+            pom {
+                name.set("Lego - Plugin")
+                description.set("Set up the basic project configuration.")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("matiasdelbel")
+                        name.set("Matias Del Bel")
+                        email.set("matiasdelbel@gmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git@github.com:matiasdelbel/lego-plugin.git")
+                    developerConnection.set("scm:git@github.com:matiasdelbel/lego-plugin.git")
+                    url.set("https://github.com/matiasdelbel/lego-plugin")
+                }
+            }
+        }
+    }
+}
+
+dependencies {
+    implementation("com.android.tools.build:gradle:7.0.4")
+
+    implementation(kotlin("gradle-plugin"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
+
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.10.0")
+    implementation("org.jlleitschuh.gradle:ktlint-gradle:10.1.0")
+}
+
+gradlePlugin.plugins.register("com.delbel.lego") {
+    id = "com.delbel.lego"
+    implementationClass = "com.delbel.lego.LegoPlugin"
+}
